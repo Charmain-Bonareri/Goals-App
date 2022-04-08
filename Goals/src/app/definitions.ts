8f2314d64c21fe487aaeac338883178a6d2a221d
Charmain <mogerecharmain@gmail.com>
2022-04-09 02:49:04 +0300

    Child components

--- Goals/src/app/definitions.ts
+++ Goals/src/app/definitions.ts
@@ -1,4 +1,6 @@
 /*
+-Our goal blueprint is goal.ts
+
 -We moved the logic from the app component to the goal component
 
 The ngIf commands simply checks if a condition is met and performs an action based on that condition.
@@ -12,4 +14,10 @@ want for the component.
 Nesting components
 -We have all our goals inside the goal component, now we make it available as a child component of the root component, appcomponent.
 -Using the goalcomponent selector,lets nest the goal component inside the appcoponent to display the contents of the goal component.
+
+-Nesting ensures the child component is loaded while inside the parent component.
+
+Child components
+-we want to give our goals additional description, we will create a goaldetail coponent that is a child of the goal component.
+-the goaldetail component will be responsible for displaying the details of each goal.
 */
