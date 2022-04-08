8f1b6847ddb64972a2245917256193ff36ca707a
Charmain <mogerecharmain@gmail.com>
2022-04-09 02:56:06 +0300

    Added descriptions to our template.

--- Goals/src/app/goal/goal.component.ts
+++ Goals/src/app/goal/goal.component.ts
@@ -10,12 +10,12 @@ export class GoalComponent implements OnInit {
 
 
   goals: Goal[] = [
-    {id:1, name:'Watch finding Nemo'},
-    {id:2, name:'Buy cookies'},
-    {id:3, name:'Get new phone case'},
-    {id:4, name:'Get dog food'},
-    {id:5, name:'Solve math homework'},
-    {id:6, name:'Plot my world domination plan'},
+    {id:1, name:'Watch finding Nemo', description:'Find an online version and watch merlin find his son'},
+    {id:2, name:'Buy cookies', description:'I have to buy cookies for the parrot'},
+    {id:3, name:'Get new phone case', description:'Diana has her birthday coming up soon'},
+    {id:4, name:'Get dog food', description:'Pupper likes expensive snacks'},
+    {id:5, name:'Solve math homework', description:'Use solving math'},
+    {id:6, name:'Plot my world domination plan', description:'Cause I am an evil overlord'},
   ];
 
   constructor() { }
