9f9ed6169dcc17422588e12fdd01f87b3f61e1ca
Charmain <mogerecharmain@gmail.com>
2022-04-09 02:24:08 +0300

    crea ted a new goal component. Imported the goal blueprint class.

---
+++ Goals/src/app/goal/goal.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Goal } from '../goal'; //imported the goal blueprint class
+
+@Component({
+  selector: 'app-goal',
+  templateUrl: './goal.component.html',
+  styleUrls: ['./goal.component.css']
+})
+export class GoalComponent implements OnInit {
+
+
+  goals: Goal[] = [
+    {id:1, name:'Watch finding Nemo'},
+    {id:2, name:'Buy cookies'},
+    {id:3, name:'Get new phone case'},
+    {id:4, name:'Get dog food'},
+    {id:5, name:'Solve math homework'},
+    {id:6, name:'Plot my world domination plan'},
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
