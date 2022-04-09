85ef0ec804ef5df90fb87063219feb2dac1af9a8
Charmain <mogerecharmain@gmail.com>
2022-04-09 03:17:21 +0300

    Created a new goal-detail component.

---
+++ Goals/src/app/goal-detail/goal-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core'; 
+
+@Component({
+  selector: 'app-goal-detail',
+  templateUrl: './goal-detail.component.html',
+  styleUrls: ['./goal-detail.component.css']
+})
+export class GoalDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
